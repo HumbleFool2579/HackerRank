bec3e35706ddf0efec7c895b8fcfc15a02693bb7
Pranav <pranav2579@gmail.com>
2015-11-22 11:45:40 +0530

    Committing from terminal

--- src/FindDigits.java
+++ src/FindDigits.java
@@ -15,7 +15,7 @@ public class FindDigits {
 
         for(i = 0;i<N;i++){
             num = s.nextLong();
-            temp = num;
+            temp = num; // testing the git commit
             count = 0;
             while(temp>0){
                 digit = temp%10;
